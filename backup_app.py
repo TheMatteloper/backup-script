685af55eb54431c9bf0ffb2651c0fd41a62a8334
TheMatteloper <154467485+TheMatteloper@users.noreply.github.com>
2025-08-08 16:40:28 +0200

    minimal viable version is probably done

--- backup_app.py
+++ backup_app.py
@@ -2,32 +2,56 @@ import os
 import argparse
 
 from src.script import traverse_directory_tree
-from src.tools import load_last_state, save_state, check_last_state
+from src.tools import load_last_state, save_state, check_if_src_dir_contains_state_file, check_and_set_src_dir, check_and_set_backup_dir
+
+# No cmd args
+def sync():
+    whole_dict = load_last_state(os.getcwd())
+
+    dict_files = whole_dict['dict_files']
+    dict_dirs = whole_dict['dict_dirs']
+    src_dir = whole_dict['root_src_dir']
+    backup_dir = whole_dict['backup_dir']
+
+    r_dict_dirs, r_dict_files = traverse_directory_tree(src_dir, backup_dir, dict_dirs, dict_files)
+    save_state(src_dir, backup_dir, r_dict_dirs, r_dict_files)
+
+    print('State file was updated')
+    print(f"Directory '{src_dir}' was successfully a backuped(just new changes, sync) to directory '{backup_dir}'")
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="Backup CLI app")
+    parser = argparse.ArgumentParser(
+        description="CLI app to backup directories and files",
+        epilog='If want to perform backup(just synchronization) of directory which already has state file, call app withou any arguments.'
+    )
 
     parser.add_argument("-v", action="store_true", help="Enable verbose output")
     parser.add_argument(
-        '--src_dir',
-        default=os.getcwd(),  # default to current working directory
-        help='Path to a directory (optional). Defaults to current working directory.'
+        '--new-backup',
+        help="Specify directory to backup and directory where should it be backuped. " \
+        "If you want to use current working directory as source, just type '.' as path. " \
+        "Both paths must be absolute and valid if specified. " \
+        "Backup directory must be empty.",
+        nargs=2,
+        metavar=("SRC_DIR", "BACKUP_DIR")
     )
-    parser.add_argument("backup_dir", help="Directory where should be source dir backuped, path must be absolute")
 
     args = parser.parse_args()
 
-    whole_dict = load_last_state(args.src_dir)
-    
-    # New dir to backup
-    if whole_dict == None:
-        r_dict_dirs, r_dict_files = traverse_directory_tree(args.src_dir, args.backup_dir)
-        save_state(args.src_dir, args.backup_dir, r_dict_dirs, r_dict_files)
+    # Check and set args
+    if args.new_backup is not None:  
+        src_dir = check_and_set_src_dir(args.new_backup[0])
+        backup_dir = check_and_set_backup_dir(args.new_backup[1])
+
+        if check_if_src_dir_contains_state_file(src_dir):
+            raise argparse.ArgumentTypeError(f"This directory is already containing state file. Can not perform new backup. Read help.")
+
+        print(f"Performing backup of new directory\n SRC DIR '{src_dir}' \n BACKUP DIR '{backup_dir}'")
+        r_dict_dirs, r_dict_files = traverse_directory_tree(src_dir, backup_dir)
+        save_state(src_dir, backup_dir, r_dict_dirs, r_dict_files)
+        print('State file was created')
+        print(f"Directory '{src_dir}' was successfully backuped to directory '{backup_dir}'")
     else:
-        # Just sync
-        check_last_state(whole_dict['root_src_dir'], whole_dict['backup_dir'], args.backup_dir)
-        dict_files = whole_dict['dict_files']
-        dict_dirs = whole_dict['dict_dirs']
-        r_dict_dirs, r_dict_files = traverse_directory_tree(args.src_dir, args.backup_dir, dict_dirs, dict_files)
-        save_state(args.src_dir, args.backup_dir, r_dict_dirs, r_dict_files)
+        sync()
+
--- src/tools.py
+++ src/tools.py
@@ -6,11 +6,16 @@ import argparse
 
 STATE_FILE_NAME = ".state_file.json"
 
+def check_if_src_dir_contains_state_file(src_dir):
+    if os.path.exists(Path(src_dir).joinpath(STATE_FILE_NAME)):
+        return True
+    return False
+
 def load_last_state(src_dir):
     if os.path.exists(Path(src_dir).joinpath(STATE_FILE_NAME)):
         with open(Path(src_dir).joinpath(STATE_FILE_NAME), 'r') as f:
             return json.load(f)
-    return None
+    raise argparse.ArgumentTypeError(f"This directory is not containing state file. You have to use --new-backup switch to backup this dir.")
 
 def save_state(root_src_dir, backup_dir, dict_dirs, dict_files):
     root_dict = {
@@ -22,12 +27,25 @@ def save_state(root_src_dir, backup_dir, dict_dirs, dict_files):
     with open(Path(root_src_dir).joinpath(STATE_FILE_NAME), 'w') as f:
         json.dump(root_dict, f, indent=2)
 
-def check_last_state(root_src_dir, backup_dir, arg_backup_dir):
-    if (Path(root_src_dir).resolve() == Path(os.getcwd()).resolve()):
-        raise argparse.ArgumentTypeError(f"'{path}' is not a valid directory")
+def check_last_state(root_src_dir, backup_dir, arg_src_dir, arg_backup_dir):
+    if (Path(root_src_dir).resolve() != Path(arg_src_dir).resolve()):
+        raise argparse.ArgumentTypeError(f"Src directory'{arg_src_dir}' is not matched with scr directory specified in state file")
     
-    if (Path(backup_dir).resolve() == Path(arg_backup_dir).resolve()):
-        return True
-    
-    return False
+    if (Path(backup_dir).resolve() != Path(arg_backup_dir).resolve()):
+        raise argparse.ArgumentTypeError(f"Backip directory'{arg_backup_dir}' is not matched with backup directory specified in state file")
 
+
+def check_and_set_src_dir(arg_src_dir):
+    if arg_src_dir == '.': 
+        return os.getcwd()
+    
+    if not os.path.exists(arg_src_dir):
+        raise argparse.ArgumentTypeError(f"'{arg_src_dir}' is not a valid src directory: directory must exist")
+
+    return arg_src_dir  
+
+def check_and_set_backup_dir(arg_backup_dir):
+    if os.path.exists(arg_backup_dir):
+        return arg_backup_dir
+    else:
+        raise argparse.ArgumentTypeError(f"'{arg_backup_dir}' is not a valid backup directory: directory must exists and must be empty")
